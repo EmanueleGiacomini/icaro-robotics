b3939de176b8fcd0a1d56cb791f12131af67e151
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-27 15:13:08 +0200

    Style\n moved library folder inside soccer folder

---
+++ soccer/phoenixsoccer/phoenixsoccer.h
@@ -0,0 +1,18 @@
+/*
+*	Phoenix-soccer library - Basic soccer library to interface with phoenix robot
+*	Made by E. Giacomini 27/08/2017.
+*	Relased under MIT License.
+*/
+
+#ifndef phoenixSoccer_h
+#define phoenixSoccer_h
+
+#include "Arduino.h"
+
+class phoenix{
+	public:
+
+	private:	
+}
+
+#endif
