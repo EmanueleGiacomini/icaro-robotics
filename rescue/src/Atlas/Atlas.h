1f706bf3083808591b8f90af33c65a32d42710e4
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-28 21:40:30 +0200

    Add movement function to Atlas library

--- rescue/src/Atlas/Atlas.h
+++ rescue/src/Atlas/Atlas.h
@@ -12,9 +12,18 @@
 #include "Arduino.h"
 
 class Atlas{
+  int _motor_A, _pwm_A;
+  int _motor_B, _pwm_B;
+  int _vel;
+
 public:
-  Atlas();
-  void move();
+  Atlas(const int motor_A, const int pwm_A, const int motor_B, const int pwm_B);
+  void goForward();
+  void goRight();
+  void goLeft();
+  void goBack();
+
+  void setSpeed(const int target_vel);
 };
 
 #endif
