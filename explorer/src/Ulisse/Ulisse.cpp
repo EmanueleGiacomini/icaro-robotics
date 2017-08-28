1dfa631832b0aef3d047d95e8bb50fc221166ed0
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-28 22:00:46 +0200

    Add movement functions to Ulisse library

--- explorer/src/Ulisse/Ulisse.cpp
+++ explorer/src/Ulisse/Ulisse.cpp
@@ -7,6 +7,42 @@
  */
 #include "Ulisse.h"
 
-Ulisse::Ulisse(){
+Ulisse::Ulisse(const int motor_A, const int pwm_A, const int motor_B, const int pwm_B){
+  pinMode(motor_A, OUTPUT);
+  pinMode(motor_B, OUTPUT);
+  pinMode(pwm_A, OUTPUT);
+  pinMode(pwm_B, OUTPUT);
+  _motor_A = motor_A;
+  _motor_B = motor_B;
+  _pwm_A = pwm_A;
+  _pwm_B = pwm_B;
 
+  _vel = 100;
+}
+void Ulisse::goForward(){
+  digitalWrite(motor_A,HIGH);
+  digitalWrite(motor_B,LOW);
+  analogWrite(_pwm_A, _vel);
+  analogWrite(_pwm_B, _vel);
+}
+void Ulisse::goRight(){
+  digitalWrite(motor_A,LOW);
+  digitalWrite(motor_B,LOW);
+  analogWrite(_pwm_A, _vel);
+  analogWrite(_pwm_B, _vel);
+}
+void Ulisse::goLeft(){
+  digitalWrite(motor_A,HIGH);
+  digitalWrite(motor_B,HIGH);
+  analogWrite(_pwm_A, _vel);
+  analogWrite(_pwm_B, _vel);
+}
+void Ulisse::goBack(){
+  digitalWrite(motor_A,LOW);
+  digitalWrite(motor_B,HIGH);
+  analogWrite(_pwm_A, _vel);
+  analogWrite(_pwm_B, _vel);
+}
+void Ulisse::setVel(const int target_vel){
+  _vel = target_vel;
 }
--- explorer/src/Ulisse/Ulisse.h
+++ explorer/src/Ulisse/Ulisse.h
@@ -12,14 +12,16 @@
 #include "Arduino.h"
 
 class Ulisse{
-
+  int _motor_A, _pwm_A;
+  int _motor_B, _pwm_B;
+  int _vel;
 public:
   Ulisse(const int motor_A, const int pwm_A, const int motor_B, const int pwm_B);
   void goForward();
   void goRight();
   void goLeft();
   void goBack();
-  void setVel();
+  void setVel(const int target_vel);
 };
 
 #endif
