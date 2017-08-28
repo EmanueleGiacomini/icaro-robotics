c82f72893b0a1e0c85ddd03150eed28c458843c0
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-28 14:23:43 +0200

    Add explorer file Add Atlas class

---
+++ explorer/explorer.ino
@@ -0,0 +1,7 @@
+/**
+ * explorer.ino
+ * Purpose: basic interface for the explorer robot
+ *
+ * @author Emanuele Giacomini
+ * @version 0.2s 27/08/17
+ */
--- rescue/src/Atlas.h
+++ rescue/src/Atlas.h
@@ -1,7 +1,7 @@
 /**
  * Atlas.h
  * Purpose: basic interface for icaro rescue robot
- * 
+ *
  * @author Emanuele Giacomini
  * @version 0.1 28/08/17
  */
@@ -9,6 +9,10 @@
 #ifndef Atlas_h
 #define Atlas_h
 
-
+class Atlas{
+public:
+  Atlas();
+  void move();
+};
 
 #endif
