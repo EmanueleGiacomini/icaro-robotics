d3210d136c4fea32d2045ec5f571649532dfd8cc
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-28 09:13:08 +0200

    Add move function

--- soccer/src/Phoenix/Phoenix.cpp
+++ soccer/src/Phoenix/Phoenix.cpp
@@ -10,8 +10,24 @@
 #include "Phoenix.h"
 
 Phoenix::Phoenix(const int* motor_pin){
-	for(int i = 0; i < sizeof(motor_pin); i++){
+	for(int i = 0; i < 4; i++){
 		_motor_pin[i] = motor_pin[i];
 		pinMode(motor_pin[i], OUTPUT);
+	}
+}
+
+void Phoenix::move(const int direction, const int rotation, const int velocity){
+	float rad_direction = (direction * 71) / 4068;
+	float vel_x = cos(rad_direction) * velocity;
+	float vel_y = sin(rad_direction) * velocity; 
+
+	float motor_vel[4] = {0, 0, 0, 0};
+
+	for(int i = 0; i < 4; i++){
+		motor_vel[i] = _motor_ang_comp[i][1] * vel_y;
+		motor_vel[i] -= _motor_ang_comp[i][0] * vel_x;
+		motor_vel[i] += rotation;
+
+		//motorDrive(i, motor_vel[i]);
 	}
 }
--- soccer/src/Phoenix/Phoenix.h
+++ soccer/src/Phoenix/Phoenix.h
@@ -10,12 +10,15 @@
 #define Phoenix_h
 
 #include "Arduino.h"
+#include "math.h"
 
 class Phoenix{
 public:
 	Phoenix(const int* motor_pin);
+	void move(const int direction, const int rotation, const int velocity);
 private:
 	int _motor_pin[4];
+	const float _motor_ang_comp[4][2] = {{0.5, -0.8660}, {0.5, 0.8660}, {-0.7071,0.7071}, {-0.7071,-0.7071}};
 
 };
 
