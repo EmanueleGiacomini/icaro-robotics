20607653bcf3d0d87b21c969b14dbb7ce7c307e0
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-08-29 21:32:37 +0200

    Add comments on phoenix functions

--- soccer/src/Phoenix/Phoenix.h
+++ soccer/src/Phoenix/Phoenix.h
@@ -34,16 +34,27 @@ class Phoenix{
 
 public:
 	/**
+		Initialize the phoenix class.
+		@param motor_pin Pointer to motor pins vector. Build the vector in this order:
+		Front-Left, Front-Right, Back-Right, Back-Left.
+		@param shift_reg_pin Pointer to shift register pins vector. Build the vector in this order:
+		Latch, Clock, Data.
 	*/
 	Phoenix(const int* motor_pin,const int* shift_reg_pin);
 	/**
+		Compute velocity for each motor in order to move to a given direction.
+		@param direction Goes from 0 to 359 where 0 is the right side of the robot.
+		Direction goes counter clockwise.
+		@param velocity Desidered velocity.
 	*/
 	void move(const int direction, const int velocity);
 	/**
+		Compute velocity for each motor in order to rotate by a given value.
+		@param rotation Determines the direction and velocity of rotation.
 	*/
 	void rotate(const int rotation);
 	/**
-
+		Send data to the motors in order to achieve the desidered roto-translation.
 	*/
 	void compute(void);
 };
