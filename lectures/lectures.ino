8645c22cec60a5e6bcbd7480c76bc391cc1da632
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-09-19 00:27:02 +0200

    Add .ino file

---
+++ lectures/lectures.ino
@@ -0,0 +1,20 @@
+/**
+  lectures.ino
+  Purpose: Test field for Icaro lectures.
+
+  @author E. Giacomini
+  @version 1.0 18/09/17
+*/
+
+#include "robotMove/robotMove.h"
+
+
+robotMove robot(1);
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+
+}
