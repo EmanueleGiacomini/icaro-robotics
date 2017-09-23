2b1eeda6976130abb5a4e45f90cea4858a2df635
EmanueleGiacomini <emanuele.giacomo@gmail.com>
2017-09-23 10:45:06 +0200

    Add some functions

---
+++ soccer/src/Phoenix_com.cpp
@@ -0,0 +1,19 @@
+/**
+  Phoenix_com.cpp
+  Purpose: Library takes care of driving comunications between modules inside Phoenix robot.
+
+  @author Emanuele Giacomini
+  @version 0.1 22/09/17
+*/
+
+Communicator::Communicator(){
+
+}
+
+Communicator::~Communicator(){
+
+}
+
+Communicator::init(uint8_t target_address){
+
+}
