79b675d3703f7b1f3d76a5ceb29dad053810ad11
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-10-12 20:53:00 +0200

    Various bugfixes =)

--- rescue/rescue.ino
+++ rescue/rescue.ino
@@ -8,16 +8,19 @@
 
 #import "src/vector_man.h"
 #import "src/rescue_sensors.h"
+#import "src/robotMove.h"
 
 const int motor_pin[4] = {12, 13, 10, 11};
 const int line_sensor_pin[3] = {A0, A1, A2};
 const int ultrasonic_pin[2] = {9, 8};
 
 rescue_line line;
+robotMove robot;
 
 void setup() {
   Serial.begin(9600);
   line.init(line_sensor_pin, 3);
+  robot.init('arduino');
 }
 
 void loop() {
