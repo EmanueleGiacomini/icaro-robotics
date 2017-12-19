e0347007fcee64bdbfa28300e2ff21174f38b6a1
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2017-12-19 21:22:30 +0100

    Add progetto_multi folder

---
+++ lectures/progetto_multi/progetto_multi.ino
@@ -0,0 +1,31 @@
+/*
+  progetto_multi.ino
+  Programma più complesso che comprende l'uso di più componenti
+*/
+
+int pin_luce = A0;
+int pin_echo = 8;
+int pin_trigger = 7;
+// 1°
+// Sviluppare funzioni per leggere il sensore di luce
+// ed il sensore di ultrasuoni.
+
+// 2°
+// Sviluppare una funzione che accende il led situato sul pin 13
+// quando vi è una fonte luminosa più forte della luce ambientale
+// (TIP: Utilizza un programma per calibrare il sensore di luce)
+
+// 3°
+// Sviluppare una funzione che, se individua un ostacolo tramite sensore ultrasonico
+// ad una distana inferiore a 5cm, accenda il led per 3 secondi (usa delay) e stampi "Ostacolo trovato"
+// sulla seriale.
+
+// Entrambi 2° e 3° devono girare contemporaneamente sul programma.
+
+void setup() {
+
+}
+
+void loop() {
+
+}
